619fae1c60130d24d5217c392f3e1b7cbdf73d5a
雅基 高野 <220034@jc-21.jp>
2023-07-07 08:43:35 +0900

    とりあえず何とかエラーを減らした

---
+++ Engine/RootJob.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Engine/GameObject.h"
+
+class RootJob :
+	public GameObject
+{
+public:
+	RootJob();
+	~RootJob();
+	void Initialize() override;
+	void Update() override;
+	void Draw() override;
+	void Release() override;
+};
+
