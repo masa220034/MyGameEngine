b609f515d91a27a4be25e6977da44d01882499d3
雅基 高野 <220034@jc-21.jp>
2023-09-01 09:13:28 +0900

    間に合った～

--- Engine/Model.cpp
+++ Engine/Model.cpp
@@ -12,21 +12,21 @@ int Model::Load(std::string fileName)
 	ModelData* pData;
 	pData = new ModelData;
 	pData->filename_ = fileName;
-	pData->pfbx_ = nullptr;
+	pData->pFbx_ = nullptr;
 
 	//ファイルネームが同じだったら、読まん！
 	for (auto& e : modelList)
 	{
 		if (e->filename_ == fileName) {
-			pData->pfbx_ = e->pfbx_;
+			pData->pFbx_ = e->pFbx_;
 			break;
 		}
 	}
 
-	if (pData->pfbx_ == nullptr)
+	if (pData->pFbx_ == nullptr)
 	{
-		pData->pfbx_ = new Fbx;
-		pData->pfbx_->Load(fileName);
+		pData->pFbx_ = new Fbx;
+		pData->pFbx_->Load(fileName);
 	}
 
 	modelList.push_back(pData);
@@ -40,7 +40,7 @@ void Model::SetTransform(int hModel, Transform transform)
 }
 void Model::Draw(int hModel) {
 	//モデル番号は、modelListのインデックス
-	modelList[hModel]->pfbx_->Draw(modelList[hModel]->transform_);
+	modelList[hModel]->pFbx_->Draw(modelList[hModel]->transform_);
 }
 
 void Model::Release()
@@ -50,7 +50,7 @@ void Model::Release()
 	{
 		for (int j = i + 1; j < modelList.size(); j++)
 		{
-			if (modelList[i]->pfbx_ == modelList[j]->pfbx_)
+			if (modelList[i]->pFbx_ == modelList[j]->pFbx_)
 			{
 				isReffered = true;
 				break;
@@ -58,7 +58,7 @@ void Model::Release()
 		}
 		if (isReffered == false)
 		{
-			SAFE_DELETE(modelList[i]->pfbx_);
+			SAFE_DELETE(modelList[i]->pFbx_);
 		}
 		SAFE_DELETE(modelList[i]);
 	}
