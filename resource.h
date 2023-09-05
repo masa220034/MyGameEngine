7c1266fdabe19469f4f21e795f7468930b677b7f
雅基 高野 <220034@jc-21.jp>
2023-09-05 11:51:06 +0900

    ダイアログ完成？

---
+++ resource.h
@@ -0,0 +1,21 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ で生成されたインクルード ファイル。
+// MapEditer.rc で使用
+//
+#define IDD_DIALOG1                     101
+#define IDC_RADIO_UP                    1001
+#define IDC_RADIO_DOWN                  1002
+#define IDC_RADIO3                      1003
+#define IDC_RADIO_CHANGE                1003
+#define IDC_COMBO1                      1005
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
