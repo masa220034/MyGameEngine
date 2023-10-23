be7129085c8c575bf92168e0a541d8aab537288c
雅基 高野 <220034@jc-21.jp>
2023-10-23 10:10:06 +0900

    また明日

--- Stage.cpp
+++ Stage.cpp
@@ -268,7 +268,7 @@ void Stage::Save()
 //セーブのルーチン
     HANDLE hFile;
     hFile = CreateFile(
-        fileName,             //ファイル名
+        fileName,               //ファイル名
         GENERIC_WRITE,          //アクセスモード（書き込み用）
         0,                      //共有（なし）
         NULL,                   //セキュリティ属性（継承しない）
@@ -276,13 +276,13 @@ void Stage::Save()
         FILE_ATTRIBUTE_NORMAL,  //属性とフラグ（設定なし）
         NULL);                  //拡張属性（なし）
 
-    std::string data = "";
+    std::string data = ; // ステージ情報を文字列として取得
 
     DWORD dwBytes = 0;          //書き込み位置
     WriteFile(                  
         hFile,                  //ファイルハンドル
-        "ABCDEF",                       //保存するデータ（文字列）
-        12,        //書き込む文字数
+        ,                       //保存するデータ（文字列）
+        ,                       //書き込む文字数
         &dwBytes,               //書き込んだサイズを入れる変数
         NULL);                  //オーバーラップド構造体（今回は使わない）
 
